402891a19c229fb6b93beb9f919230dba9684642
Sun Shine <inshallrajpoot826@gmail.com>
2026-08-16 22:09:04 +0500

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  // Must match the GitHub repository name so built asset URLs resolve
+  // correctly when served from https://<user>.github.io/house-coffee-desserts/
+  base: '/house-coffee-desserts/',
   plugins: [react()],
 })
